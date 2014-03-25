9de7c99c193b128a5b4b17eb754acec46f767b55
Nicolas Brousse <pro@nicolas-brousse.fr>
2014-03-25 21:03:01 +0100

    Upgrade Composer/Script

--- lib/TwigIntegratorKit/Composer/Script.php
+++ lib/TwigIntegratorKit/Composer/Script.php
@@ -14,24 +14,38 @@ class Script
         $filesystem = new Filesystem();
         $finder     = new Finder();
 
+        self::chmod($dir . '/web/css', 0777);
+        self::chmod($dir . '/web/js', 0777);
+        self::chmod($dir . '/app/cache', 0777);
+
+        $finder->directories()
+            ->depth('== 0')
+            ->in($dir . '/integration/public')
+            ->exclude('css')
+            ->exclude('js')
+        ;
+
+        foreach ($finder as $file) {
+            self::symlink($file->getRealpath(), $dir . '/web/' . $file->getRelativePathname());
+        }
+    }
+
+    private static function chmod($dir, $mode)
+    {
+        $filesystem = new Filesystem();
+
         try {
-            $filesystem->chmod($dir . '/web/css', 0777);
-            $filesystem->chmod($dir . '/web/js', 0777);
-            $filesystem->chmod($dir . '/app/cache', 0777);
+            $filesystem->chmod($dir, $mode);
         }
         catch (\Exception $e) {}
+    }
+
+    private static function symlink($origin, $target)
+    {
+        $filesystem = new Filesystem();
 
         try {
-            $finder->directories()
-                ->depth('== 0')
-                ->in($dir . '/integration/public')
-                ->exclude('css')
-                ->exclude('js')
-            ;
-
-            foreach ($finder as $file) {
-                $filesystem->symlink($file->getRealpath(), 'web/' . $file->getRelativePathname());
-            }
+            $filesystem->symlink($origin, $target);
         }
         catch (\Exception $e) {}
     }
