380d79b919e8e4c684d21951748ff6cc576fd43a
Nicolas Brousse <nicolas@opsone.net>
2014-07-23 11:59:44 +0200

    Update Composer/Script.php
    * Add a Tmp Fix for assets symlink
    * Make it more verbose

--- lib/TwigIntegratorKit/Composer/Script.php
+++ lib/TwigIntegratorKit/Composer/Script.php
@@ -2,13 +2,16 @@
 
 namespace TwigIntegratorKit\Composer;
 
+use Composer\Script\Event;
+
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Finder\Finder;
 
 class Script
 {
-    public static function install()
+    public static function install(Event $event)
     {
+        $io  = $event->getIO();
         $dir = realpath(__DIR__ . '/../../../');
 
         $filesystem = new Filesystem();
@@ -17,6 +20,12 @@ class Script
         self::chmod($dir . '/web/css', 0777);
         self::chmod($dir . '/web/js', 0777);
         self::chmod($dir . '/app/cache', 0777);
+
+        $io->write('<info>Update twig-integrator web/* chmod</info>');
+
+        // Temporary Fix
+        // TODO: choose between /assets symlink or /assets/* symlinks
+        self::symlink($dir . '/integration/public', $dir . '/web/assets');
 
         $finder->directories()
             ->depth('== 0')
@@ -28,6 +37,8 @@ class Script
         foreach ($finder as $file) {
             self::symlink($file->getRealpath(), $dir . '/web/' . $file->getRelativePathname());
         }
+
+        $io->write('<info>Generate twig-integrator symlinks</info>');
     }
 
     private static function chmod($dir, $mode)
